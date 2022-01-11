070949c09bb8951956d958cadc590e78f51a1f8a
Corvin Deboeser <corvin@quod.ai>
2022-01-11 01:26:11 +0100

    Add static serving

--- server/server.js
+++ server/server.js
@@ -113,5 +113,12 @@ app.put(
   })
 );
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../build/index.html"));
+  });
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
